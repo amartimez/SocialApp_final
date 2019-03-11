df981759d76edd3cc6f199eede9e687e71c8cc3c
amartimez <amartimez@elpuig.xeill.net>
2019-03-11 20:22:04 +0100

    final

--- app/src/main/java/com/example/gerard/socialapp/view/fragment/PostsFragment.java
+++ app/src/main/java/com/example/gerard/socialapp/view/fragment/PostsFragment.java
@@ -11,9 +11,10 @@ import android.view.ViewGroup;
 
 import com.example.gerard.socialapp.GlideApp;
 import com.example.gerard.socialapp.R;
+import com.example.gerard.socialapp.view.activity.PostComentActivity;
 import com.example.gerard.socialapp.model.Post;
-import com.example.gerard.socialapp.view.PostViewHolder;
 import com.example.gerard.socialapp.view.activity.MediaActivity;
+import com.example.gerard.socialapp.view.adapter.PostsAdapter;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
@@ -42,67 +43,7 @@ public class PostsFragment extends Fragment {
 
         RecyclerView recycler = view.findViewById(R.id.rvPosts);
         recycler.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recycler.setAdapter(new FirebaseRecyclerAdapter<Post, PostViewHolder>(options) {
-            @Override
-            public PostViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-                LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
-                return new PostViewHolder(inflater.inflate(R.layout.item_post, viewGroup, false));
-            }
-
-            @Override
-            protected void onBindViewHolder(final PostViewHolder viewHolder, int position, final Post post) {
-                final String postKey = getRef(position).getKey();
-
-                viewHolder.author.setText(post.author);
-                GlideApp.with(PostsFragment.this).load(post.authorPhotoUrl).circleCrop().into(viewHolder.photo);
-
-                if (post.likes.containsKey(mUser.getUid())) {
-                    viewHolder.like.setImageResource(R.drawable.heart_on);
-                    viewHolder.numLikes.setTextColor(getResources().getColor(R.color.red));
-                } else {
-                    viewHolder.like.setImageResource(R.drawable.heart_off);
-                    viewHolder.numLikes.setTextColor(getResources().getColor(R.color.grey));
-                }
-
-                viewHolder.content.setText(post.content);
-
-                if (post.mediaUrl != null) {
-                    viewHolder.image.setVisibility(View.VISIBLE);
-                    if ("audio".equals(post.mediaType)) {
-                        viewHolder.image.setImageResource(R.drawable.audio);
-                    } else {
-                        GlideApp.with(PostsFragment.this).load(post.mediaUrl).centerCrop().into(viewHolder.image);
-
-                    }
-                    viewHolder.image.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            Intent intent = new Intent(getActivity(), MediaActivity.class);
-                            intent.putExtra("mediaUrl", post.mediaUrl);
-                            intent.putExtra("mediaType", post.mediaType);
-                            startActivity(intent);
-                        }
-                    });
-                } else {
-                    viewHolder.image.setVisibility(View.GONE);
-                }
-
-                viewHolder.numLikes.setText(String.valueOf(post.likes.size()));
-
-                viewHolder.likeLayout.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        if (post.likes.containsKey(mUser.getUid())) {
-                            mReference.child("posts/data").child(postKey).child("likes").child(mUser.getUid()).setValue(null);
-                            mReference.child("posts/user-likes").child(mUser.getUid()).child(postKey).setValue(null);
-                        } else {
-                            mReference.child("posts/data").child(postKey).child("likes").child(mUser.getUid()).setValue(true);
-                            mReference.child("posts/user-likes").child(mUser.getUid()).child(postKey).setValue(true);
-                        }
-                    }
-                });
-            }
-        });
+        recycler.setAdapter(new PostsAdapter(options, getContext(), mUser, mReference));
 
         return view;
     }
